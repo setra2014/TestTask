99bb3edb3f296d0cefc13771c0c803596f2c764d
setra2014 <36576562+setra2014@users.noreply.github.com>
2019-03-18 14:41:23 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+
 import java.util.concurrent.TimeUnit;
 
 
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public MediaPlayer playerArr[] = new MediaPlayer[2];
     public TextView textForSeekBar;
     public SeekBar seekBar;
-    final Handler handlerForToast = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +40,29 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TaskPlayClick task = new TaskPlayClick();                   //создание отдельного потока для воспроизведения треков
-                task.execute();
+                TaskClickPlay task = new TaskClickPlay();                       //основной поток воспроизведения треков
+                int CrossFade = (seekBar.getProgress()+2)*1000;                 //длительность склейки
+
+                try {
+                    int FirstAudioDuration = playerArr[0].getDuration();
+                    int SecondAudioDuration = playerArr[1].getDuration();
+
+                    //если треки слишком коротки, то выводится соответвующее сообщение
+                    if (FirstAudioDuration > 2 * CrossFade && SecondAudioDuration > 2 * CrossFade) {
+                        unlockInterface(false);
+                        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    }
+                    else{
+                        Toast toast = Toast.makeText(getApplicationContext(), "Аудиодорожки коротки!", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+                catch (NullPointerException e) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Аудиодорожка не выбрана", Toast.LENGTH_SHORT);
+                    toast.show();
+                    unlockInterface(true);
+                }
+
             }
         });
 
@@ -66,6 +86,112 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         });
 
     }
+
+    //основной поток, вызывающийся при нажатии на кнопку Play
+    public class TaskClickPlay extends AsyncTask <Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... arg0){
+            TaskFirstAudio taskFirst;                                           //поток воспроизведения первого трека
+            TaskSecondAudio taskSecond;                                         //поток воспроизведения второго трека
+            int flag = 1;                                                       //флаг, нужный для определения того, в первый ли раз проигрывается цикл
+            int sleepDuration = 1000 * (seekBar.getProgress() + 2);             //длительность ожидания для начала новой итерации цикла
+            try {
+                while (true) {
+                    taskFirst = new TaskFirstAudio();
+                    taskSecond = new TaskSecondAudio();
+
+                    taskFirst.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, flag);
+
+                    taskSecond.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
+                    try { TimeUnit.MILLISECONDS.sleep(sleepDuration); } catch (InterruptedException e) { }
+                    flag = 0;
+                    sleepDuration = 3000 * (seekBar.getProgress() + 2);
+                }
+            }catch (NullPointerException e){}
+            return null;
+        }
+    }
+
+    //поток воспроизведения первого трека
+    public class TaskFirstAudio extends AsyncTask <Integer, Void, Void>{
+        final int CrossFade = (seekBar.getProgress()+2) * 1000;                 //длительность склейки
+
+        @Override
+        protected Void doInBackground(Integer... flag){
+            int sleepDuration = playerArr[0].getDuration() - (2-flag[0])*CrossFade;    //длительность ожидания завершения потока
+
+            playerArr[0].start();
+            if(flag[0] != 1) increaseVolume(0, CrossFade);                //если это не первое воспроизведение этого потока,
+            else playerArr[0].setVolume(1, 1);              // то звук плавно увеличивается
+
+            try{ TimeUnit.MILLISECONDS.sleep(sleepDuration);} catch (InterruptedException e) {}
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result){
+            new Thread(){
+                @Override
+                public void run(){
+                    reduceVolume(0, CrossFade);                            //при завершении потока звук плавно затухает
+                }
+            }.start();
+        }
+    }
+
+    //поток воспроизведения второго трека
+    public class TaskSecondAudio extends AsyncTask <Void, Void, Void>{
+        final int CrossFade = (seekBar.getProgress()+2) * 1000;                 //длительность склейки
+        int sleepDuration = playerArr[1].getDuration() - 2*CrossFade;           //длительность ожидания завершения потока
+
+        @Override
+        protected Void doInBackground(Void... arg0){
+            playerArr[1].start();
+            increaseVolume(1, CrossFade);                                 //плавное увеличение звука
+            try{ TimeUnit.MILLISECONDS.sleep(sleepDuration);} catch (InterruptedException e){}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result){
+            new Thread(){
+                @Override
+                public void run(){
+                    reduceVolume(1, CrossFade);                           //плавное затухание звука
+                }
+            }.start();
+        }
+    }
+
+
+    //метод разблокировки/блокировки элементов интерфейса
+    public void unlockInterface(boolean bool){
+        seekBar.setEnabled(bool);
+        play.setEnabled(bool);
+        getFirstAudio.setEnabled(bool);
+        getSecondAudio.setEnabled(bool);
+    }
+
+    //метод плавного увеличения громкости
+    public void increaseVolume(int index, int CrossFade){
+
+        for (float i = 0.1f ; i <= 1; i+=0.1f) {
+            playerArr[index].setVolume(i, i);
+            try {
+                TimeUnit.MILLISECONDS.sleep(CrossFade / 10);
+            } catch (InterruptedException e) { }
+        }
+    }
+
+    //метод плавного уменьшения громкости
+    public void reduceVolume(int index, int CrossFade){
+        for (float i = 0.9f; i >= 0; i-=0.1f) {
+            playerArr[index].setVolume(i, i);
+            try {
+                TimeUnit.MILLISECONDS.sleep((CrossFade) / 10);
+            } catch (InterruptedException e) { }
+        }
+    }
+
 
     //переопределение метода возврата данных в нашу активность
     @Override
@@ -92,148 +218,4 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar){}
-
-
-    //отдельный поток для воспроизведения треков
-    public class TaskPlayClick extends AsyncTask <Void, Void, Void>{
-        //блокировка кнопок и SeekBar'a
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            unlockInterface(false);
-        }
-
-        //цикличное воспроизведение треков
-        @Override
-        protected Void doInBackground(Void...arg0){
-            try {
-                //воспроизведение первого трека без увеличесния громкости в начале
-                TaskBegin taskBegin = new TaskBegin();
-                taskBegin.execute();
-
-                //цикл воспроизведения треков с кроссфейдом
-                while (true) {
-                    TaskSecondAudio taskSecond = new TaskSecondAudio();
-                    taskSecond.execute();
-
-                    TaskFirstAudio taskFirst = new TaskFirstAudio();
-                    taskFirst.execute();
-                }
-            }
-            catch (NullPointerException e){
-                //выводим сообщение об ошибке
-                handlerForToast.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast toast = Toast.makeText(getApplicationContext(), "Вы не выбрали аудиодорожку!", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                });
-            }
-            return null;
-        }
-
-
-        //разблокировка кнопок и SeekBar'a
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            unlockInterface(true);
-        }
-    }
-
-    //поток для воспроизведения 1 трека без увеличения громкости в начале
-    public class TaskBegin extends AsyncTask <Void, Void, Void> {
-
-        final int CrossFade = (seekBar.getProgress()+2) * 1000;             //длительность кроссфейда
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            playerArr[0].start();
-            playerArr[0].setVolume(1,1);
-            int sleepDuration = playerArr[0].getDuration() - CrossFade;
-            try { TimeUnit.MILLISECONDS.sleep(sleepDuration); } catch (InterruptedException e) {}
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            reduceVolume(0, CrossFade);
-            return null;
-        }
-    }
-
-    //поток для воспроизведения 1 трека с кроссфейдом
-    public class TaskFirstAudio extends AsyncTask <Void, Void, Void> {
-
-        final int CrossFade = (seekBar.getProgress()+2) * 1000;         //длительность кроссфейда
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            playerArr[0].setVolume(0, 0);
-            playerArr[0].start();
-            increaseVolume(0, CrossFade);                           //плавное увеличение громкости
-            int sleepDuration = playerArr[0].getDuration() - 2*CrossFade;
-            try {
-                TimeUnit.MILLISECONDS.sleep(sleepDuration);
-            } catch (InterruptedException e) {}
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            reduceVolume(0, CrossFade);                             //плавное уменьшение громкости
-            return null;
-        }
-    }
-
-    //поток для воспроизведения 2 трека
-    public class TaskSecondAudio extends AsyncTask <Void, Void, Void>{
-        final int CrossFade = (seekBar.getProgress()+2)*1000;             //длительность кроссфейда
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            playerArr[1].setVolume(0,0);
-            playerArr[1].start();
-            increaseVolume(1, CrossFade);                           //плавное увеличение громкости
-            long sleepDuration = playerArr[1].getDuration() - 2*CrossFade;
-            try{ TimeUnit.MILLISECONDS.sleep(sleepDuration);} catch (InterruptedException e){}
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            reduceVolume(1, CrossFade);                             //плавное уменьшение громкости
-            return null;
-        }
-    }
-
-    //метод разблокировки/блокировки элементов интерфейса
-    public void unlockInterface(boolean bool){
-        seekBar.setEnabled(bool);
-        play.setEnabled(bool);
-        getFirstAudio.setEnabled(bool);
-        getSecondAudio.setEnabled(bool);
-    }
-
-    //метод плавного увеличения громкости
-    public void increaseVolume(int index, int CrossFade){
-        for (float i = 0.1f ; i <= 1; i+=0.1f) {
-            playerArr[index].setVolume(i, i);
-            try {
-                TimeUnit.MILLISECONDS.sleep(CrossFade / 10);
-            } catch (InterruptedException e) { }
-        }
-    }
-
-    //метод плавного уменьшения громкости
-    public void reduceVolume(int index, int CrossFade){
-        for (float i = 0.9f; i >= 0; i-=0.1f) {
-            playerArr[index].setVolume(i, i);
-            try {
-                TimeUnit.MILLISECONDS.sleep(CrossFade / 10);
-            } catch (InterruptedException e) { }
-        }
-    }
 }
